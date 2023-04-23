c98b7826e31a23b571004ebcf4c1873e7699a892
Artem Cherenkov <a.cherenkov@cian.ru>
2023-04-23 20:29:20 +0700

    исправил кейс, при котором модалка закрывалась, если при скролле остановить палец на одно месте и продолжить скроллить

--- src/components/mk3/index.tsx
+++ src/components/mk3/index.tsx
@@ -1,12 +1,12 @@
 import "./styles.css";
 import { animated, useSpring } from "@react-spring/web";
-import { useDrag } from "@use-gesture/react";
+import { Handler, useGesture } from "@use-gesture/react";
 import {
   MutableRefObject,
   useCallback,
   useEffect,
   useRef,
-  useState
+  useState,
 } from "react";
 import cn from "classnames";
 
@@ -66,55 +66,93 @@ export const Mk3 = () => {
     [maxHeight]
   );
 
+  let dragMy = 0;
+  let elScroll: undefined | number = undefined;
 
-  const bind = useDrag(
-    ({ active, movement: [, my], offset: [, oy] }) => {
+  const toggleBodyScroll = (isLocked: boolean) => {
+    if (isLocked) {
+      document.body.style.overflow = "hidden";
+      document.body.style.overscrollBehavior = "none";
+    } else {
+      document.body.style.overflow = "auto";
+      document.body.style.overscrollBehavior = "auto";
+    }
+  };
 
+  const dragFn: Handler<
+    "drag",
+    PointerEvent | MouseEvent | TouchEvent | KeyboardEvent
+  > = ({ cancel, active, movement: [, my], offset: [, oy] }) => {
+    dragMy = my;
 
-      if (modalState.current === EState.FULL && my > 0) {
+    if (elScroll && elScroll > 10) {
+      cancel();
+    }
+
+    if (my < -70) {
+      if (oy > getHeightForState(EState.HALF)) {
         modalState.current = EState.HALF;
-        swipeRef.current?.classList.remove("swipe_scroll");
+      } else {
+        modalState.current = EState.FULL;
       }
-
-      if (my < -70) {
-        if (oy > getHeightForState(EState.HALF)) {
-          modalState.current = EState.HALF;
-        } else {
-          modalState.current = EState.FULL;
-        }
-      } else if (my > 70) {
-        if (oy < getHeightForState(EState.HALF)) {
-          modalState.current = EState.HALF;
-        } else {
-          modalState.current = EState.HIDDEN;
-        }
+    } else if (my > 70) {
+      if (oy < getHeightForState(EState.HALF)) {
+        modalState.current = EState.HALF;
+      } else {
+        modalState.current = EState.HIDDEN;
       }
+    }
 
-      if (modalState.current > EState.HIDDEN) {
-        document.body.style.overflow = "hidden";
-        document.body.style.overscrollBehavior = "none";
+    toggleBodyScroll(modalState.current > EState.HIDDEN);
+
+    if (swipeRef.current) {
+      if (modalState.current === EState.FULL) {
+        swipeRef.current.classList.add("swipe_scroll");
+      } else {
+        swipeRef.current.classList.remove("swipe_scroll");
       }
+    }
 
-      if (swipeRef.current) {
-        if (modalState.current === EState.FULL) {
-          swipeRef.current.classList.add("swipe_scroll");
-        } else {
-          swipeRef.current.classList.remove("swipe_scroll");
-        }
-      }
+    api.start({
+      y: active ? oy : getHeightForState(modalState.current),
+      immediate: false,
+    });
+  };
 
-      api.start({
-        y: active ? oy : getHeightForState(modalState.current),
-        immediate: false
-      });
+  const scrollFn: Handler<"scroll", UIEvent> = ({ event }) => {
+    // @ts-ignore
+    const scrollTop = event.currentTarget?.scrollTop;
+    const isFullMode = modalState.current === EState.FULL;
+    elScroll = scrollTop;
+
+    if (isFullMode && dragMy > 0 && scrollTop < 5) {
+      modalState.current = EState.HALF;
+      swipeRef.current?.classList.remove("swipe_scroll");
+    }
+
+    api.start({
+      y: getHeightForState(modalState.current),
+      immediate: false,
+    });
+  };
+
+  const bind = useGesture(
+    {
+      onDrag: dragFn,
+      onScroll: scrollFn,
     },
     {
-      filterTaps: true,
-      from: () => [0, y.get()],
-      rubberband: true
+      drag: {
+        filterTaps: true,
+        from: () => [0, y.get()],
+        rubberband: true,
+      },
+      scroll: {
+        filterTaps: true,
+        from: () => [0, y.get()],
+      },
     }
   );
-
   useEffect(() => {
     const headerHeight = headerRef.current?.offsetHeight || 0;
     api.start({ y: -headerHeight });
